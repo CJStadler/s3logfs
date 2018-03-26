1410545badc6ead9d4d04f8d25a444d5066428b5
bchayes <hayes.br@husky.neu.edu>
2018-03-26 12:58:44 -0400

    adjusted to 6 bytes for segmentid
    I adjusted this so that we will have an 8 byte address, this will cleanly fit into a 4096 block 512 times. I thought about it more and 6 bytes for segmentid should be fine, that allows for 140 trillion segments. If we were writing 5 segments a second it would take 887,000+ years to run out of segmentid's. Even if storage is x1000 faster it would be good for 887 years.

--- s3logfs/fs/blockaddress.py
+++ s3logfs/fs/blockaddress.py
@@ -4,12 +4,12 @@ class BlockAddress:
   #  - bytearray (1 argument)
   #  - segmentid,offset (2 arguments)
   def __init__(self, *args, **kwargs):
-    # 10-byte bytearray (8 = segmentid, 2 = offset)
+    # 8-byte bytearray (6 = segmentid, 2 = offset)
     if (len(args) == 1):  # bytearray
-      # first 8 bytes is segmentid
-      self.segmentid = struct.unpack("<Q", args[0][0:8])[0]
-      # last 2 bytes is offset
-      self.offset = struct.unpack("<H", args[0][8:10])[0]
+      # first 6 bytes is segmentid (placed in unsigned long long)
+      self.segmentid = struct.unpack("<Q", args[0][0:6] + b'\x00\x00')[0]
+      # last 2 bytes is offset (placed in unsigned short)
+      self.offset = struct.unpack("<H", args[0][6:8])[0]
     elif (len(args) == 2):  # segmentid & offset
       # first argument is segmentid
       self.segmentid = args[0]
@@ -20,8 +20,8 @@ class BlockAddress:
       # *** need to replace this with how we will handle errors ***
       print("ERROR")
   
-  # returns BlockAddress as a list of 10 bytes
+  # returns BlockAddress as a list of 8 bytes
   def to_bytes(self):
-    b1 = (self.segmentid).to_bytes(8, byteorder='little', signed=False)
+    b1 = (self.segmentid).to_bytes(6, byteorder='little', signed=False)
     b2 = (self.offset).to_bytes(2, byteorder='little', signed=False)
-    return b1[0:8] + b2[0:2]
+    return b1[0:6] + b2[0:2]
