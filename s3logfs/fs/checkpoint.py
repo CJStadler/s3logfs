412143523a777a92e3521ac5bd71720bb7fa7ef5
Brian Hayes <hayes.br@husky.neu.edu>
2018-03-31 00:29:05 -0400

    adjusted default file system sizing

--- s3logfs/fs/checkpoint.py
+++ s3logfs/fs/checkpoint.py
@@ -6,8 +6,8 @@ class CheckpointRegion:
 
   def __init__(self):
     self.blockSize = 4096          # bytes
-    self.segmentSize = 512         # blocks
-    self.fsSize = 2**15            # blocks
+    self.segmentSize = 512         # blocks (default 2MB)
+    self.fsSize = 2**28            # blocks (default 1TB space)
     self.fsUsed = 0                # blocks
     self.segmentCounter = 0        # unsigned long long (limited to 6 bytes)
     self.iNodeCounter = 0          # unsigned long long
