d122cc35849425240a34800fc1d6a5107d7e800f
Brian Hayes <hayes.br@husky.neu.edu>
2018-03-30 23:41:56 -0400

    Checkpoint Region

--- s3logfs/fs/__init__.py
+++ s3logfs/fs/__init__.py
@@ -1,4 +1,7 @@
 from .blockaddress import BlockAddress
+from .superblock import SuperBlock
+from .checkpoint import CheckpointRegion
 from .inode import INode
 from .log import Log
 from .segment import ReadOnlySegment, ReadWriteSegment
+
--- s3logfs/fs/checkpoint.py
+++ s3logfs/fs/checkpoint.py
@@ -1,7 +1,17 @@
 import struct
 from blockaddress import BlockAddress
 from collections import defaultdict
+import pickle
 
 class CheckpointRegion:
+
   def __init__(self):
+    self.blockSize = 4096          # bytes
+    self.segmentSize = 512         # blocks
+    self.fsSize = 2**15            # blocks
+    self.fsUsed = 0                # blocks
+    self.segmentCounter = 0        # unsigned long long (limited to 6 bytes)
+    self.iNodeCounter = 0          # unsigned long long
     self.iNodeMap = defaultdict()
+
+
