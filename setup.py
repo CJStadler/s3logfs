c09f85a461801641d329138fbe000b6e8b70aed2
Chris Stadler <chrisstadler@gmail.com>
2018-04-26 10:50:07 -0400

    Bump version to 0.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='0.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -69,7 +69,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.ccs.neu.edu/bchayes/cs5600project',  # Optional
+    url='https://github.com/CJStadler/s3logfs',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
